3f3ce7774d61cb7c7cb53fa27741cf26f937a0a6
SAMS-univ <123590713+SAMS-univ@users.noreply.github.com>
2023-01-25 20:29:38 +0100

    Create javascriptweb.js

---
+++ english_1a2/javascriptweb.js
@@ -0,0 +1,16 @@
+//Cambio de imagen del logo al pasar el cursor (By ChatGPT)
+/*
+$(document).ready(function(){
+    $(".logotipeimg").hover(function(){
+        $(this).animate({"opacity": 0}, "slow", function(){
+            $(this).attr("src", "mediaBox/DALL·E 2022-12-07 16.08.46 - A dark background with an image of a group of three red triangles fitting together. Digital art.png");
+            $(this).animate({"opacity": 1}, "slow");
+        });
+    }, function(){
+        $(this).animate({"opacity": 0}, "slow", function(){
+            $(this).attr("src", "mediaBox/Captura de pantalla_20230121_154517.png");
+            $(this).animate({"opacity": 1}, "slow");
+        });
+    });
+});
+*/
