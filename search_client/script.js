699b59d9bd721dc6f8f0b46dbbae0047233cdec3
SAMSuniv <123590713+SAMSuniv@users.noreply.github.com>
2023-06-13 15:17:51 +0200

    Update script.js

--- search_client/script.js
+++ search_client/script.js
@@ -4,7 +4,7 @@ document.getElementById("search-form").addEventListener("submit", function (even
     searchFiles(searchTerm);
 });
 
-function searchFiles(searchTerm) {
+/*function searchFiles(searchTerm) {
     var resultsContainer = document.getElementById("search-results");
     resultsContainer.innerHTML = "";
 
@@ -51,6 +51,47 @@ function getPageText(pdf, pageNumber) {
                     return item.str;
                 }).join(" ");
                 resolve(pageText);
+            });
+        });
+    });
+}*/
+
+
+
+function searchFiles(searchTerm) {
+    var resultsContainer = document.getElementById("search-results");
+    resultsContainer.innerHTML = "";
+
+    // Array con las URLs de los archivos PDF
+    var pdfUrls = [
+        "ruta/al/archivo1.pdf",
+        "ruta/al/archivo2.pdf",
+        "ruta/al/archivo3.pdf"
+        // Agrega más URLs de archivos PDF aquí
+    ];
+
+    pdfUrls.forEach(function (pdfUrl) {
+        // Cargar y procesar el archivo PDF
+        PDFJS.getDocument(pdfUrl).promise.then(function (pdf) {
+            var totalNumPages = pdf.numPages;
+            var searchPromises = [];
+
+            for (var i = 1; i <= totalNumPages; i++) {
+                searchPromises.push(getPageText(pdf, i));
+            }
+
+            Promise.all(searchPromises).then(function (pageTexts) {
+                var searchText = pageTexts.join(" ").toLowerCase();
+                if (searchText.includes(searchTerm)) {
+                    var resultElement = document.createElement("div");
+                    resultElement.classList.add("result");
+                    var linkElement = document.createElement("a");
+                    linkElement.setAttribute("href", pdfUrl);
+                    linkElement.setAttribute("target", "_blank"); // Abrir el enlace en una nueva pestaña
+                    linkElement.textContent = pdfUrl;
+                    resultElement.appendChild(linkElement);
+                    resultsContainer.appendChild(resultElement);
+                }
             });
         });
     });
