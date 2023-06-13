490fbb86c58bbd038798bd4e4671de361bf4c608
SAMSuniv <123590713+SAMSuniv@users.noreply.github.com>
2023-06-13 15:22:42 +0200

    Update script.js

--- search_client/script.js
+++ search_client/script.js
@@ -64,25 +64,27 @@ function searchFiles(searchTerm) {
 
     // Array con las URLs de los archivos PDF
     var pdfUrls = [
-        "samsuniv.github.io/english_1a2/mediaBox/works/502011a.pdf",
+        "ruta/al/archivo1.pdf",
         "ruta/al/archivo2.pdf",
-        "ruta/al/archivo3.pdf"
+        "../english_1a2/mediaBox/works/502011a.pdf"
         // Agrega más URLs de archivos PDF aquí
     ];
 
     pdfUrls.forEach(function (pdfUrl) {
         // Cargar y procesar el archivo PDF
         PDFJS.getDocument(pdfUrl).promise.then(function (pdf) {
-            var totalNumPages = pdf.numPages;
             var searchPromises = [];
 
-            for (var i = 1; i <= totalNumPages; i++) {
-                searchPromises.push(getPageText(pdf, i));
+            for (var i = 1; i <= pdf.numPages; i++) {
+                searchPromises.push(getPageText(pdf, i, searchTerm));
             }
 
-            Promise.all(searchPromises).then(function (pageTexts) {
-                var searchText = pageTexts.join(" ").toLowerCase();
-                if (searchText.includes(searchTerm)) {
+            Promise.all(searchPromises).then(function (searchResults) {
+                var filteredResults = searchResults.filter(function (result) {
+                    return result !== null;
+                });
+
+                if (filteredResults.length > 0) {
                     var resultElement = document.createElement("div");
                     resultElement.classList.add("result");
                     var linkElement = document.createElement("a");
@@ -91,6 +93,24 @@ function searchFiles(searchTerm) {
                     linkElement.textContent = pdfUrl;
                     resultElement.appendChild(linkElement);
                     resultsContainer.appendChild(resultElement);
+                }
+            });
+        });
+    });
+}
+
+function getPageText(pdf, pageNumber, searchTerm) {
+    return new Promise(function (resolve, reject) {
+        pdf.getPage(pageNumber).then(function (page) {
+            page.getTextContent().then(function (textContent) {
+                var pageText = textContent.items.map(function (item) {
+                    return item.str;
+                }).join(" ");
+                
+                if (pageText.toLowerCase().includes(searchTerm.toLowerCase())) {
+                    resolve(pageText);
+                } else {
+                    resolve(null);
                 }
             });
         });
